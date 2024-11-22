ea6e4ae690ece19726866361c62b90ceebeed467
rikyhidayat21 <rikyhidayat21@gmail.com>
2024-11-22 15:36:52 +0700

    refactor to not using instance variable if not needed

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,37 +4,37 @@ module Api
       before_action :set_user, except: [ :index, :create ]
 
       def index
-        @users = User.select(:id, :name)
+        users = User.select(:id, :name)
 
-        render json: @users
+        render json: users
       end
 
       def create
-        @user = User.new(user_params)
+        user = User.new(user_params)
 
-        if @user.save
-          render json: @user, status: :created
+        if user.save
+          render json: user, status: :created
         else
-          render json: { errors: @user.errors }, status: :unprocessable_entity
+          render json: { errors: user.errors }, status: :unprocessable_entity
         end
       end
 
       def clock_in
-        @sleep_record = @user.sleep_records.build(clock_in: Time.current)
+        sleep_record = @user.sleep_records.build(clock_in: Time.current)
 
-        if @sleep_record.save
-          render json: @sleep_record, status: :created
+        if sleep_record.save
+          render json: sleep_record, status: :created
         else
-          render json: { errors: @sleep_record.errors }, status: :unprocessable_entity
+          render json: { errors: sleep_record.errors }, status: :unprocessable_entity
         end
       end
 
       def clock_out
-        @sleep_record = @user.sleep_records.where(clock_out: nil).order(created_at: :desc).first
+        sleep_record = @user.sleep_records.where(clock_out: nil).order(created_at: :desc).first
 
-        if @sleep_record
-          @sleep_record.update(clock_out: Time.current)
-          render json: @sleep_record
+        if sleep_record
+          sleep_record.update(clock_out: Time.current)
+          render json: sleep_record
         else
           render json: { error: "No active sleep record found" }, status: :not_found
         end
