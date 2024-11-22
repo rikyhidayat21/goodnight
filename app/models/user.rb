ec3b2360e291fd14d9d03455f3cfda6b55aade06
rikyhidayat21 <rikyhidayat21@gmail.com>
2024-11-22 16:55:41 +0700

    enhance User model association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
   has_many :sleep_records
 
+  has_many :active_follows, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_follows, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_follows, source: :followed
+  has_many :followers, through: :passive_follows, source: :follower
+
   validates :name, presence: true
 end
