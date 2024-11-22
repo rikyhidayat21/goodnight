557c98aa6bd732c42c45f5bd269855e508751ff5
rikyhidayat21 <rikyhidayat21@gmail.com>
2024-11-22 15:55:23 +0700

    add callback calculate sleep duration

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -1,3 +1,11 @@
 class SleepRecord < ApplicationRecord
   belongs_to :user
+
+  before_save :calculate_sleep_duration, if: :clock_out?
+
+  private
+
+  def calculate_sleep_duration
+    self.duration_minutes = ((clock_out - clock_in) / 60).to_i
+  end
 end
