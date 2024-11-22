f055b868a621ba52306f6c3e8c1135456a2cb3dd
rikyhidayat21 <rikyhidayat21@gmail.com>
2024-11-22 15:09:52 +0700

    create users controller and make index & create action work

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,27 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+        @users = User.select(:id, :name)
+
+        render json: @users
+      end
+
+      def create
+        @user = User.new(user_params)
+
+        if @user.save
+          render json: @user, status: :created
+        else
+          render json: { errors: @user.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:name)
+      end
+    end
+  end
+end
